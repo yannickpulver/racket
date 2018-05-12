b14a7976db34299b02794217fc26007f1da26581
Yannick Pulver <hi@yanu.me>
2018-05-12 15:19:34 +0200

    Updated pages

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import {MatchPage} from "../match/match";
+import {RankingPage} from "../ranking/ranking";
+import {TeamPage} from "../team/team";
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = MatchPage;
+  tab2Root = RankingPage;
+  tab3Root = TeamPage;
 
   constructor() {
 
